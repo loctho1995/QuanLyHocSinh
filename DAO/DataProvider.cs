06626e7f8c7f556b2f621d1c14bd6acd86f2809c
Khắc Chinh <13520079@gm.uit.edu.vn>
2015-03-13 09:51:45 +0700

    database
    update database

---
+++ DAO/DataProvider.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAO
+{
+    public class DataProvider
+    {
+        public static SqlConnection KetNoi()
+        {
+            string sChuoiketnoi = @"Data Source=HP;Initial Catalog=SQL_QUANLYHOCSINH;Integrated Security=TRUE;User ID=HP\KHACCHINH;Password=";
+            SqlConnection con = new SqlConnection(sChuoiketnoi);
+            con.Open();
+            return con;
+
+        }
+        public static void DongKetNoiDatabase(SqlConnection con)
+        {
+            con.Close();
+        }
+        public static DataTable LayDataTable(string sTruyvan, SqlConnection con)
+        {
+            SqlDataAdapter da = new SqlDataAdapter(sTruyvan, con);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            return dt;
+        }
+        public static void ThucthiTruyVanNonQuery(string sTruyvan, SqlConnection con)
+        {
+            SqlCommand cm = new SqlCommand(sTruyvan, con);
+            cm.ExecuteNonQuery();
+        }
+    }
+}
